65a9976f31b9ceea5bef5134d41a495bc5e298b1
Callum Wilcox <thebug950@gmail.com>
2026-03-28 15:40:56 -0600

    Minor grammatical fix

--- COMP-4513-Assignment-2-react-app/vite.config.js
+++ COMP-4513-Assignment-2-react-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'COMP-4513-Assignment-2',
+  base: '/COMP-4513-Assignment-2/',
   plugins: [react()],
 });
