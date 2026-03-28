c0881a802c0ed75a177f6767a37b75e55b2a04e5
Callum Wilcox <thebug950@gmail.com>
2026-03-28 13:39:42 -0600

    Updated app.jsx

--- COMP-4513-Assignment-2-react-app/src/App.jsx
+++ COMP-4513-Assignment-2-react-app/src/App.jsx
@@ -1,26 +1,37 @@
-import { AppShell } from "@mantine/core";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import AppRouter from "./router/AppRouter";
-
-import { AuthProvider } from "./context/AuthContext";
-import { PlaylistProvider } from "./context/PlaylistContext";
-import { DataProvider } from "./context/DataContext";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+import Artists from './pages/Artists';
+import Genres from './pages/Genres';
+import Browse from './pages/Browse';
+import SongDetails from './pages/SongDetails';
+import ArtistDetails from './pages/ArtistDetails';
+import GenreDetails from './pages/GenreDetails';
+import Playlists from './pages/Playlists';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import { useAuth } from './context/AuthContext';
 
 export default function App() {
+  const { isLoggedIn } = useAuth();
+
   return (
-    <AuthProvider>
-      <PlaylistProvider>
-        <DataProvider>
-          <AppShell
-            header={<Header />}
-            footer={<Footer />}
-            padding="md"
-          >
-            <AppRouter />
-          </AppShell>
-        </DataProvider>
-      </PlaylistProvider>
-    </AuthProvider>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/genres" element={<Genres />} />
+        <Route path="/songs" element={<Browse />} />
+        <Route path="/songs/:id" element={<SongDetails />} />
+        <Route path="/artists/:id" element={<ArtistDetails />} />
+        <Route path="/genres/:id" element={<GenreDetails />} />
+        <Route
+          path="/playlists"
+          element={isLoggedIn ? <Playlists /> : <Navigate to="/login" replace />}
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
   );
 }
