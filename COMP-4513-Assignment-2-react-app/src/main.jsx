da2542b9042f0128a817257f859358bb860d0c13
Callum Wilcox <thebug950@gmail.com>
2026-03-28 13:36:17 -0600

    Added node modules/beginning modules

--- COMP-4513-Assignment-2-react-app/src/main.jsx
+++ COMP-4513-Assignment-2-react-app/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
+  </React.StrictMode>
+);
